cb0f9e2be652e49305cc9290d22682a9060413ed
bollamr3 <bollamr3@coventry.ac.uk>
2025-04-03 02:02:29 +0530

    Day 2 : Edited EventServlet.java file

--- Event_Managemet_Platform/src/com/eventapp/servlet/EventServlet.java
+++ Event_Managemet_Platform/src/com/eventapp/servlet/EventServlet.java
@@ -1,1 +1,21 @@
-// Event servlet code will be added here later
+package com.eventapp.servlet;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+// This servlet handles GET requests for the /event path
+@WebServlet("/event")
+public class EventServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("text/html; charset=UTF-8");
+
+        String message = "<h2>Hello! You have reached the Event Page via Servlet.</h2>";
+        res.getWriter().write(message);
+    }
+}
