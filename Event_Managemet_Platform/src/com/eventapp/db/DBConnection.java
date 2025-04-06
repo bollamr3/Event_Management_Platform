ffd4a6b28e73db3544e05d27c16727f5d7116d14
bollamr3 <bollamr3@coventry.ac.uk>
2025-04-06 13:53:36 +0530

    Day 4 Updated DBConnection.java

--- Event_Managemet_Platform/src/com/eventapp/db/DBConnection.java
+++ Event_Managemet_Platform/src/com/eventapp/db/DBConnection.java
@@ -1,1 +1,23 @@
-// Code for database connection will go here
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConn() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Updated JDBC URL
+            String url =  "jdbc:mysql://localhost:3306/event_db";
+
+            String username = "root";
+            String password = "20122001@DOBpass";
+
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace(); 
+        }
+        return conn;
+    }
+}
